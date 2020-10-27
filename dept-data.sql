77096d48aa0d705664249e8d1932cd9d7b0d107c
yishengxin <957956791@qq.com>
2020-10-27 17:20:20 +0800

    company1027

--- dept-data.sql
+++ dept-data.sql
@@ -1,7 +1,5 @@
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (1, '开发部', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (2, '人事部', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (3, '财务部', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (4, '市场部', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (5, '运维部', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (8, 'bigData2018', 'clouddb01');
-INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (10, 'AI', 'clouddb01');
+INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (1, '开发部', DATABASE());
+INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (2, '人事部', DATABASE());
+INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (3, '财务部', DATABASE());
+INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (4, '市场部', DATABASE());
+INSERT INTO `dept`(`deptno`, `dname`, `db_source`) VALUES (5, '运维部', DATABASE());
